6cf51006b799b0f197f34ca49749370d08faf002
iceBear67 <icebear67@sfclub.cc>
2022-01-02 12:36:05 +0800

    Add mapper for CatchingContext

--- src/main/java/io/ib67/util/CatchingContext.java
+++ src/main/java/io/ib67/util/CatchingContext.java
@@ -1,42 +1,61 @@
 package io.ib67.util;
 
+import java.util.function.BiFunction;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class CatchingContext<T> {
-  private Throwable failure;
-  private T result;
-  public CatchingContext(T result){
-    this.result=result;
-  }
-  public CatchingContext(Throwable failure){
-    this.failure=failure;
-  }
-  public CatchingContext<T> alsoPrintStack(){
-    if(failure!=null){
-      failure.printStackTrace();
+    private Throwable failure;
+    private T result;
+
+    public CatchingContext(T result) {
+        this.result = result;
     }
-    return this;
-  }
 
-  public CatchingContext<T> onFailure(Consumer<Throwable> consumer) {
-    if (failure != null) {
-      consumer.accept(failure);
+    public CatchingContext(Throwable failure) {
+        this.failure = failure;
     }
-    return this;
-  }
 
-  public boolean isFailed() {
-    return failure != null;
-  }
-
-  public T getResult() {
-    return result;
-  }
-
-  public CatchingContext<T> onSuccess(Consumer<T> consumer) {
-    if (result != null) {
-      consumer.accept(result);
+    public CatchingContext<T> alsoPrintStack() {
+        if (failure != null) {
+            failure.printStackTrace();
+        }
+        return this;
     }
-    return this;
-  }
+
+    public CatchingContext<T> onFailure(Consumer<Throwable> consumer) {
+        return onFailure((t, r) -> {
+            consumer.accept(failure);
+            return r;
+        });
+    }
+
+    public CatchingContext<T> onFailure(BiFunction<Throwable, T, T> mapper) {
+        if (failure != null) {
+            result = mapper.apply(failure, result);
+        }
+        return this;
+    }
+
+    public boolean isFailed() {
+        return failure != null;
+    }
+
+    public T getResult() {
+        return result;
+    }
+
+    public CatchingContext<T> onSuccess(Consumer<T> consumer) {
+        return onSuccess((r) -> {
+            consumer.accept(r);
+            return r;
+        });
+    }
+
+    public CatchingContext<T> onSuccess(Function<T, T> consumer) {
+        if (result != null) {
+            result = consumer.apply(result);
+        }
+        return this;
+    }
 }

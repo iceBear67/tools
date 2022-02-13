f8dd245ae9f5f0fcece023621b9bd928da0bd341
iceBear67 <icebear67@sfclub.cc>
2022-02-13 14:11:27 +0800

    Add readFromConfig

--- src/main/java/io/ib67/util/bukkit/Texts.java
+++ src/main/java/io/ib67/util/bukkit/Texts.java
@@ -1,14 +1,20 @@
 package io.ib67.util.bukkit;
 
+import io.ib67.Util;
 import me.clip.placeholderapi.PlaceholderAPI;
 import org.bukkit.entity.Player;
 
+import java.util.Optional;
 import java.util.function.UnaryOperator;
 
 // constants.
 public class Texts {
+
     public static final UnaryOperator<String> placeholderApi(Player player) {
         return t -> PlaceholderAPI.setPlaceholders(player, "%" + t + "%");
     }
 
+    public static final UnaryOperator<String> readFromConfig() {
+        return t -> Optional.ofNullable(Util.BukkitAPI.currentPlugin().getConfig().get(t)).map(Object::toString).orElse("null");
+    }
 }

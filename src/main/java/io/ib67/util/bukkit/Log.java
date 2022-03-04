f88e7646e99212329ffc80caabfb382ac505ae7a
iceBear67 <icebear67@sfclub.cc>
2022-03-04 21:51:27 +0800

    Add log via module

--- src/main/java/io/ib67/util/bukkit/Log.java
+++ src/main/java/io/ib67/util/bukkit/Log.java
@@ -7,13 +7,23 @@ import org.bukkit.ChatColor;
 
 public class Log {
     public static Text pattern = Text.of("&b%s&8: &f");
+    public static Text modulePattern = Text.of("&b%s/%m&8: &f");
     private static Lazy<?, String> realPrefix = Lazy.by(() -> pattern.toString().replace("%s", Util.BukkitAPI.currentPlugin().getName()));
+    private static Lazy<String, String> modulePrefix = Lazy.by(moduleName -> pattern.toString().replace("%s", Util.BukkitAPI.currentPlugin().getName()));
 
     public static void info(String message) {
         Bukkit.getConsoleSender().sendMessage(realPrefix.get() + Text.colored(message));
     }
 
+    public static void info(String module, String message) {
+        Bukkit.getConsoleSender().sendMessage(modulePrefix.get().replace("%m", module) + Text.colored(message));
+    }
+
     public static void warn(String message) {
         Bukkit.getConsoleSender().sendMessage(realPrefix.get() + ChatColor.RED + Text.colored(message));
+    }
+
+    public static void warn(String module, String message) {
+        Bukkit.getConsoleSender().sendMessage(modulePrefix.get().replace("%m", module) + ChatColor.RED + Text.colored(message));
     }
 }

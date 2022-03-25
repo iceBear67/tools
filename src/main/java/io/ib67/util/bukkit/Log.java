10254f1e10d021bb8fcf8e273d8ddcaf0a98f1a0
iceBear67 <icebear67@sfclub.cc>
2022-03-25 22:08:44 +0800

    Fix: log#info(module,message) doesn't display module

--- src/main/java/io/ib67/util/bukkit/Log.java
+++ src/main/java/io/ib67/util/bukkit/Log.java
@@ -9,8 +9,9 @@ public class Log {
     public static Text pattern = Text.of("&b%s&8: &f");
     public static Text modulePattern = Text.of("&b%s/%m&8: &f");
     private static Lazy<?, String> realPrefix = Lazy.by(() -> pattern.toString().replace("%s", Util.BukkitAPI.currentPlugin().getName()));
-    private static Lazy<String, String> modulePrefix = Lazy.by(moduleName -> pattern.toString().replace("%s", Util.BukkitAPI.currentPlugin().getName()));
+    private static Lazy<String, String> modulePrefix = Lazy.by(moduleName -> modulePattern.toString().replace("%s", Util.BukkitAPI.currentPlugin().getName()));
 
+    @Deprecated
     public static void info(String message) {
         Bukkit.getConsoleSender().sendMessage(realPrefix.get() + Text.colored(message));
     }
